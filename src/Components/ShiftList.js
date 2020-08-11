69180357c8d626add766b4ea7f94c8d9b76b040a
Safeer Hayat <shayat@and.digital>
2020-08-11 19:19:58 +0100

    Formats and filters api data

--- src/Components/ShiftList.js
+++ src/Components/ShiftList.js
@@ -1,17 +1,51 @@
 import React, { useEffect, useState } from "react";
+import moment from "moment";
 import { getShiftData } from "../api/shiftApi";
+import { locum } from "../userConfig/user";
 
-function ShiftList() {
+const ShiftList = () => {
+    const shiftData = useFormattedShiftData();
+    return shiftData.map((shift) => <div>{shift.practiceName}</div>);
+};
+
+const useFormattedShiftData = () => {
     const [shiftData, setShiftData] = useState([]);
     useEffect(() => {
         const getApiData = async () => {
             const apiResult = await getShiftData();
-            setShiftData(apiResult.data);
+            const formattedData = apiResult.data
+                .filter(filterShiftData)
+                .map(formatShiftData);
+
+            setShiftData(formattedData);
         };
         getApiData();
     }, []);
 
-    return shiftData.map((shift) => <div>{shift.practice.name}</div>);
-}
+    return shiftData;
+};
+
+const formatShiftData = (shift) => {
+    const shiftDate = moment(shift.startDatetime).format("YYYY-MM-DD");
+    const startTime = moment(shift.startDatetime).format("HH:mm");
+    const endTime = moment(shift.endDatetime).format("HH:mm");
+    return {
+        practiceName: shift.practice.name,
+        shiftDate,
+        startTime,
+        endTime,
+        hourlyRate: shift.hourlyRate,
+        noOfApplications: shift.applicationIds.length
+    };
+};
+
+const filterShiftData = (shift) => {
+    const now = moment();
+    if (shift.staffTypeId !== locum.staffTypeId) return false;
+    if (!moment(shift.startDatetime).isAfter(now)) return false;
+    if (shift.status !== "POSTED") return false;
+    if (shift.locum && Object.keys(shift.locum).length > 0) return false;
+    return true;
+};
 
 export default ShiftList;
---
+++ src/userConfig/user.js
@@ -0,0 +1,7 @@
+export const locum = {
+    id: "1234",
+    firstName: "John",
+    lastName: "Doe",
+    staffType: "gp",
+    staffTypeId: "1"
+};
